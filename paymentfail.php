9c2125fb84ed1588d29b0184397ffa032cd0c08c
JavaCakez <tom.hargrave19@gmail.com>
2015-08-28 19:28:04 +0100

    Payment fail & success pages refactor

--- paymentfail.php
+++ paymentfail.php
@@ -1,118 +1,48 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Failed Payment | ZOOQIE</title>
-<meta name="viewport" content="width=1000">
-<meta name="description" content="Discover the best up and coming independent clothing brands from around the UK. Shop from skate, surf, snow, urban, vintage and many more styles from many different brands with more being added every day. Zooqie is home to independent clothing brands you won&#39;t find anywhere else all in one place.">
-<link rel="icon" href="favicon.ico" type="image/x-icon">
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <head>
+        <title>Failed Payment | ZOOQIE</title>
+        <meta name="description" content="Discover the best up and coming independent clothing brands from around the UK. Shop from skate, surf, snow, urban, vintage and many more styles from many different brands with more being added every day. Zooqie is home to independent clothing brands you won&#39;t find anywhere else all in one place.">
+        <?
+            //Variable declarations
+            $folderLevel = 0;
+            $folderString = '';
+            $names = array('Home', 'Payment Failed');
+            $links = array('index.php', 'paymentfail.php');
+            $pageHeight = 700;
 
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="js/jquery.js"></script>
+            include($folderString . 'php/head.php');
+        ?>
 
+        <style type="text/css">
+            .Body-C
+            {
+                font-family:"Lato", sans-serif; color:#2c2c2c; font-size:14px; line-height:1.29em;
+            }
+            .Heading-1-C
+            {
+                font-family:"Harabara", serif; color:#656565; font-size:24px; line-height:1.50em;
+            }
+        </style>
+    </head>
 
-<!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
+    <body>
+        <div class="pageWrapper">
+            <? include($folderString . 'php/header.php'); ?>
+            <div class="pageContent" style="height:<?echo $pageHeight;?>px;">
+                <? include($folderString . 'php/navBar.php'); ?>
 
-<meta http-equiv="Content-Style-Type" content="text/css">
-<meta http-equiv="Content-Script-Type" content="text/javascript">
+                <div id="txt_2" style="position:absolute;left:20px;top:155px;width:960px;height:52px;overflow:hidden;">
+                <h1 class="Wp-Heading-1-P" style="margin-top:0px"><span class="Heading-1-C">Payment Failed</span></h1>
+                </div>
+                <div id="txt_12" style="position:absolute;left:20px;top:219px;width:960px;height:105px;overflow:hidden;">
+                <p class="Wp-Body-P"><span class="Body-C">Your payment was unsuccessful. Please ensure you entered all your information correctly
+                    and try again.</span></p>
+                <p class="Wp-Body-P"><span class="Body-C">If problems persist, please feel free to <a href="contact.php" style="text-decoration:underline;">contact us</a>.</span></p>
+                </div>
 
-    <?php
-    //Include database settings
-    if(file_exists("db_settings.php")) {include("db_settings.php");}
-    else if(file_exists("../db_settings.php")) {include("../db_settings.php");}
-    else if(file_exists("../../db_settings.php")) {include("../../db_settings.php");}
-
-    //Include utilities
-    if(file_exists("utils.php")) {include("utils.php");}
-    else if(file_exists("../utils.php")) {include("../utils.php");}
-    else if(file_exists("../../utils.php")) {include("../../utils.php");}
-    ?>
-
-
-
-
-
-
-
-<style type="text/css">
-body{margin:0;padding:0;}
-.wpfixed{position:absolute;}
-div > .wpfixed{position:fixed;}
-a.hlink_1:link {color:#2c2c2c;}
-a.hlink_1:visited {color:#2c2c2c;}
-a.hlink_1:hover {color:#e52b50;}
-a.hlink_1:active {color:#2c2c2c;}
-.Heading-2-C
-{
-    font-family:"Harabara", serif; color:#656565; font-size:19px; line-height:1.47em;
-}
-.Body-C
-{
-    font-family:"Lato", sans-serif; color:#2c2c2c; font-size:14px; line-height:1.29em;
-}
-.Heading-2-C-C0
-{
-    font-family:"Harabara", serif; color:#656565; font-size:21px; line-height:1.48em;
-}
-.Heading-1-C
-{
-    font-family:"Harabara", serif; color:#656565; font-size:24px; line-height:1.50em;
-}
-
-</style>
-
-
-<script type="text/javascript">
-
-
-</script>
-</head>
-
-<?
-//height of page excluding footer
-$pageHeight = 466;
-// 222 is footer height
-$tmpHeight = $pageHeight + 222;
-?>
-
-
-<body text="#000000" style="background:#ffffff url('images/backgroundpattern.png') repeat fixed top center; height:<?echo $tmpHeight;?>px; /*Master Page Body Style*/ -webkit-box-shadow:1 1px 15px rgba(0,0,0,0.3); box-shadow:0 1px 15px rgba(0,0,0,0.3);">
-<!--Master Page Body Start-->
-
-<?php
-echoFooter(0, $pageHeight);
-echoFacebookScript();
-echoHeader(0, $tmpHeight);
-echoSocialMediaFollowButtons();
-echoGoogleAnalyticsScript();
-echoNavBar(0, array('Home', 'Payment Failed'), array('index.php', 'paymentfail.php'));
-?>
-
-
-
-
-
-
-
-<div id="txt_2" style="position:absolute;left:20px;top:155px;width:960px;height:52px;overflow:hidden;">
-<h1 class="Wp-Heading-1-P" style="margin-top:0px"><span class="Heading-1-C">Payment Failed</span></h1>
-</div>
-<div id="txt_12" style="position:absolute;left:20px;top:219px;width:960px;height:105px;overflow:hidden;">
-<p class="Wp-Body-P"><span class="Body-C">Your payment was unsuccessful. Please ensure you entered all your information correctly
-    and try again.</span></p>
-<p class="Wp-Body-P"><span class="Body-C">If problems persist, please feel free to <a href="contact.php" style="text-decoration:underline;">contact us</a>.</span></p>
-</div>
-<!--Master Page End-->
-<div id="nav-bar"></div>
-</div>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/totop.min.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
-<!--Page Body End-->
-
-
-</body>
+            </div>
+            <? include($folderString . 'php/footer.php'); ?>
+        </div>
+    </body>
 </html>
-

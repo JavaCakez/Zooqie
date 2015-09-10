3ae5228196f622f2255e3c9ea43a5aec2cab6170
JavaCakez <tom.hargrave19@gmail.com>
2015-09-10 11:37:21 +0100

    Loginmeu refactor

--- loginmenu.php
+++ loginmenu.php
@@ -1,315 +1,63 @@
-<?
-ob_start (); // Buffer output
-?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title><!--TITLE--></title>
-<meta name="viewport" content="width=1000">
-<link rel="icon" href="favicon.ico" type="image/x-icon">
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="js/jquery.js"></script>
-
-
-<!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
-
-
-<meta http-equiv="Content-Style-Type" content="text/css">
-<meta http-equiv="Content-Script-Type" content="text/javascript">
-<meta content="<!--DESCRIPTION-->" name="description" property="og:description" />
-
-    <?php
-    //Include database settings
-    if(file_exists("db_settings.php")) {include("db_settings.php");}
-    else if(file_exists("../db_settings.php")) {include("../db_settings.php");}
-    else if(file_exists("../../db_settings.php")) {include("../../db_settings.php");}
-
-    //Include utilities
-    if(file_exists("utils.php")) {include("utils.php");}
-    else if(file_exists("../utils.php")) {include("../utils.php");}
-    else if(file_exists("../../utils.php")) {include("../../utils.php");}
-    ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<style type ="text/css">
-.Heading-1-P
-{
-    margin:32px 0px 4px 0px; text-align:center; font-weight:400;
-}
-.Heading-2-C
-{
-    font-family:"Harabara", serif; color:#656565; font-size:19px; line-height:1.47em;
-}
-.Heading-2-CC
-{
-    font-family:"Harabara", serif; color:#3A3A3A; font-size:22px; line-height:1.47em;
-}
-.Heading-2-CD
-{
-    font-family:"Harabara", serif; color:#3A3A3A; font-size:15px; line-height:1.47em;
-}
-.Body-C
-{
-    font-family:"Lato", sans-serif; color:#2c2c2c; font-size:14px; line-height:1.29em;
-}
-.Heading-2-C-C0
-{
-    font-family:"Harabara", serif; color:#656565; font-size:21px; line-height:1.48em;
-}
-.Heading-3-C
-{
-    font-family:"Harabara", serif; color:#e52b50; font-size:14px; line-height:1.50em;
-}
-.Heading-1-C-C0
-{
-    font-family:"Harabara", serif; color:#656565; font-size:19px; line-height:1.47em;
-}
-.Heading-1-C-C10
-{
-    font-family:"Harabara", serif; color:#656565; font-size:24px; line-height:1.47em;
-}
-.Heading-1-C-C1
-{
-    font-family:"Harabara", serif; color:#656565; font-size:13px; line-height:1.54em;
-}
-.Heading-3-C-C0
-{
-    font-family:"Harabara", serif; font-weight:700; color:#656565; font-size:16px; line-height:1.50em;
-}
-.Heading-3-C-C1
-{
-    font-family:"Harabara", serif; color:#656565; font-size:13px; line-height:1.54em;
-}
-.Heading-3-C-C10
-{
-    font-family:"Harabara", serif; color:#656565; font-size:13px; line-height:1.54em; text-align: center;
-}
-.Heading-1-C-C9
-{
-    font-family:"Harabara", serif; color:#656565; font-size:27px; line-height:1.47em;
-}
-.Body-C-C3
-{
-    font-family:"Lato", sans-serif; color:#2c2c2c; font-size:16px; line-height:1.38em;
-}
-.Body-C-C4
-{
-    font-family:"Lato", sans-serif; color:#ff0000; font-size:16px; line-height:1.38em;
-}
-.Heading-3-C-C12
-{
-    font-family:"Harabara", serif; font-size:15px; line-height:1.47em;
-}
-</style>
-
-
-
-
-
-
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-
-
-<style type="text/css">
-body{margin:0;padding:0;}
-.wpfixed{position:absolute;}
-div > .wpfixed{position:fixed;}
-a.hlink_1:link {color:#2c2c2c;}
-a.hlink_1:visited {color:#2c2c2c;}
-a.hlink_1:hover {color:#e52b50;}
-a.hlink_1:active {color:#2c2c2c;}
-.Heading-2-C
-{
-    font-family:"Harabara", serif; color:#656565; font-size:19px; line-height:1.47em;
-}
-.Body-C
-{
-    font-family:"Lato", sans-serif; color:#2c2c2c; font-size:14px; line-height:1.29em;
-}
-.Heading-2-C-C0
-{
-    font-family:"Harabara", serif; color:#656565; font-size:21px; line-height:1.48em;
-}
-.Heading-1-C
-{
-    font-family:"Harabara", serif; color:#656565; font-size:19px; line-height:1.47em;
-}
-.Body-C-C0
-{
-    font-family:"Harabara", serif; color:#2c2c2c; font-size:32px; line-height:1.47em;
-}
-
-</style>
-
-
-<script type="text/javascript">
-
-
-</script>
-</head>
-
-
-<body text="#000000" style="background:#ffffff url('images/backgroundpattern.png') repeat fixed top center; height:<!--PAGEHEIGHTVAL1-->px; /*Master Page Body Style*/ -webkit-box-shadow:1 1px 15px rgba(0,0,0,0.3); box-shadow:0 1px 15px rgba(0,0,0,0.3); "  >
-
-<!--Master Page Body Start-->
-
-<?php
-echoFooter(0, '<!--PAGEHEIGHTVAL-->');
-echoFacebookScript();
-echoHeader(0, '<!--PAGEHEIGHTVAL1-->');
-echoSocialMediaFollowButtons();
-echoGoogleAnalyticsScript();
-?>
-
-//TODO: what is this page?!?! do we need it?
-
-<img src="images/navbar.png" border="0" width="1000" height="40" id="qs_1" alt="Navigation Bar" style="position:absolute;left:0px;top:80px;" >
-<!--NAVBAR-->
-
-
-
-
-
-
-
-
-
-<div style="position:relative; top:134px; width:100%;">
-	
-	<img src="images\storeimages\THC_background2.jpg" alt="" width="1000" height="577" />
-	
-	<div style = "position:absolute; top: 80px; left: 370px; width: 270px; text-align: center;background-color:#ececec; opacity:.85">
-		<span class="Body-C-C0">Brand Login Menu</span>
-	</div>
-	
-	<div style ="position:absolute; top:250px; width:500px; float: left; text-align: center; background-color:#ececec; opacity:.85">
-	<br><span class="Heading-2-CC"><a class = "hlink_1" style = "text-decoration:none;" href = "uploadmenu.php"> Upload Tool</a></span>
-	<br><span class="Heading-2-CD">Upload and edit products and store appearance</span></br><br>
-	</div>
-
-	<div style ="position:absolute; top:250px; left:500px; width:500px; text-align: center; background-color:#ececec; opacity:.85">
-	<br><span class="Heading-2-CC"><a class = "hlink_1" style = "text-decoration:none;" href = "dashboard.php"> Dashboard</span> </a>
-	<br><span class="Heading-2-CD">View sales and views performance </span></br><br>
-	</div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--Master Page End-->
-<div id="nav-bar"></div>
-
-</div>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/totop.min.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
-<!--Page Body End-->
-
-</body>
+    <head>
+        <title>Brand Log In Menu | ZOOQIE</title>
+        <?
+            //Variable declarations
+            $folderLevel = 0;
+            $folderString = '';
+            $names = array('Home', 'Log In Menu');
+            $links = array('index.php', 'loginmenu.php');
+            $pageHeight = 617;
+
+            include($folderString . 'php/head.php');
+        ?>
+
+
+        <style type ="text/css">
+            .Heading-2-CC
+            {
+                font-family:"Harabara", serif; color:#3A3A3A; font-size:22px; line-height:1.47em;
+            }
+            .Heading-2-CD
+            {
+                font-family:"Harabara", serif; color:#3A3A3A; font-size:15px; line-height:1.47em;
+            }
+            .Body-C-C0
+            {
+                font-family:"Harabara", serif; color:#2c2c2c; font-size:32px; line-height:1.47em;
+            }
+        </style>
+    </head>
+
+
+    <body >
+        <div class="pageWrapper">
+            <? include($folderString . 'php/header.php'); ?>
+            <div class="pageContent" style="height:<?echo $pageHeight;?>px;">
+                <? include($folderString . 'php/navBar.php'); ?>
+
+                <div style="position:relative; width:100%;">
+                    <img src="images\storeimages\THC_background2.jpg" alt="" width="1000" height="577" />
+
+                    <div style = "position:absolute; top: 80px; left: 370px; width: 270px; text-align: center;background-color:#ececec; opacity:.85">
+                        <span class="Body-C-C0">Brand Login Menu</span>
+                    </div>
+
+                    <div style ="position:absolute; top:250px; width:500px; float: left; text-align: center; background-color:#ececec; opacity:.85">
+                    <br><span class="Heading-2-CC"><a class = "hlink_1" style = "text-decoration:none;" href = "uploadmenu.php"> Upload Tool</a></span>
+                    <br><span class="Heading-2-CD">Upload and edit products and store appearance</span></br><br>
+                    </div>
+
+                    <div style ="position:absolute; top:250px; left:500px; width:500px; text-align: center; background-color:#ececec; opacity:.85">
+                    <br><span class="Heading-2-CC"><a class = "hlink_1" style = "text-decoration:none;" href = "dashboard.php"> Dashboard</span> </a>
+                    <br><span class="Heading-2-CD">View sales and views performance </span></br><br>
+                    </div>
+                </div>
+
+            </div>
+            <? include($folderString . 'php/footer.php'); ?>
+        </div>
+    </body>
 </html>
 
-
-
-
-
-
-
-
-<?
-$pageContents = ob_get_contents (); // Get all the page's HTML into a string
-ob_end_clean (); // Wipe the buffer
-
-$navBar = '
-
-<div class="nav_348style" id="nav_348" style="left: 20px; top: 91px;; width: 960px; height: 26px; position: absolute;">
-	<a id="nav_348_I0" href="index.php" target="_self"> Home </a>
-	 &gt; 
-	<a id="nav_348_I2" href="loginmenu.php" target="_self"> Login Menu </a>
-</div>';
-
-
-$minHeight = 720;
-$ph = $b + 40;
-if($ph < $minHeight) $ph = $minHeight;
-$pageHeight = $ph ;
-$pageHeightVal1 = $ph  + 222;
-$pageHeightVal2 = $ph  + 14;
-$pageHeightVal3 = $ph  + 65;
-$pageHeightVal4 = $ph  + 162;
-$pageHeightVal5 = $ph  + 183;
-$pageHeightVal6 = $ph  + 115;
-
-
-// Replace <!--TITLE--> with $pageTitle variable contents, and print the HTML
-$array1 = array("<!--TITLE-->", '<!--NAVBAR-->', '<!--PAGEHEIGHT-->', '<!--PAGEHEIGHTVAL1-->', '<!--PAGEHEIGHTVAL2-->', '<!--PAGEHEIGHTVAL3-->', '<!--PAGEHEIGHTVAL4-->', '<!--PAGEHEIGHTVAL5-->', '<!--PAGEHEIGHTVAL6-->', '<!--DESCRIPTION-->');//Values to replace 
-$array2 = array("Brand Login Menu | zooqie ", $navBar, $pageHeight, $pageHeightVal1, $pageHeightVal2, $pageHeightVal3, $pageHeightVal4, $pageHeightVal5, $pageHeightVal6, $d);  //Replacements. 
-
-$str = str_replace($array1, $array2, $pageContents);
-echo $str;
-?>
